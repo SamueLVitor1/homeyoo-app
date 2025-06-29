566516c4f2a3b64d083bdaa34627610a6d22bb0c
Samuel Vitor <samuelvitor668@gmail.com>
2025-06-29 01:13:42 -0300

    feat: implementa layout inicial da tela da casa com tarefas e ranking

--- src/screens/usuario-com-casa-screen/index.tsx
+++ src/screens/usuario-com-casa-screen/index.tsx
@@ -1,11 +1,122 @@
-import { View, Text } from "react-native"
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { styles } from './styles'
+import { Feather } from '@expo/vector-icons'
 
-export function UsuarioComCasa({ casa }: any) {
+export function UsuarioComCasa() {
+  // Dados mock
+  const casa = {
+    nome: 'Casa Legal',
+    codigo: '#B3up1',
+    membros: [
+      { nome: 'Alex', avatar: require('../../assets/icon-exemple.png') },
+      { nome: 'Anna', avatar: require('../../assets/icon-exemple.png') },
+      { nome: 'Emma', avatar: require('../../assets/icon-exemple.png') }
+    ],
+    tarefas: [
+      { titulo: 'Tarefa exemplo', responsavel: 'Anna', status: 'Em andamento', avatar: require('../../assets/icon-exemple.png') },
+      { titulo: 'Tarefa exemplo', responsavel: 'Anna', status: 'Atrasado', avatar: require('../../assets/icon-exemple.png') },
+      { titulo: 'Tarefa exemplo', responsavel: 'Anna', status: 'Atrasado', avatar: require('../../assets/icon-exemple.png') },
+      { titulo: 'Tarefa exemplo', responsavel: 'Anna', status: 'Atrasado', avatar: require('../../assets/icon-exemple.png') }
+    ],
+    ranking: [
+      { nome: 'Alex', xp: 87, avatar: 'https://i.pravatar.cc/150?img=5' },
+      { nome: 'Emma', xp: 110, avatar: 'https://i.pravatar.cc/150?img=8' },
+      { nome: 'Anna', xp: 57, avatar: 'https://i.pravatar.cc/150?img=9'  }
+    ],
+    metaXp: 200
+  }
+
   return (
-    <View style={{ padding: 24 }}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
-        casa
-      </Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={{ backgroundColor: "#add9b6", flex: 1 }}>
+        <Image
+          source={require("../../assets/house-header.png")}
+          style={styles.headerImage}
+          resizeMode="cover"
+        />
+
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <Text style={styles.nomeCasa}>{casa.nome}</Text>
+            <Text style={styles.codigoCasa}>{casa.codigo}</Text>
+          </View>
+
+          <Text style={styles.sectionTitle}>
+            Membros
+          </Text>
+          {/* Membros */}
+          <View style={styles.membrosContainer}>
+            {casa.membros.map((m, i) => (
+              <View key={i} style={styles.membroItem}>
+                <Image source={m.avatar} style={styles.avatar} />
+                <Text>{m.nome}</Text>
+              </View>
+            ))}
+          </View>
+
+          {/* Tarefas */}
+          <View style={styles.tarefasContainer}>
+            <Text style={styles.sectionTitle}>Tarefas</Text>
+            <TouchableOpacity style={styles.novaTarefaButton}>
+              <Feather name="plus-square" size={16} color="#4338CA" />
+              <Text style={styles.novaTarefaText}>Nova tarefa</Text>
+            </TouchableOpacity>
+            {casa.tarefas.map((t, i) => (
+              <View key={i} style={styles.tarefaItem}>
+                <View>
+                  <Text style={styles.tituloTarefa}>{t.titulo}</Text>
+                  <View style={styles.responsavelContainer}>
+                    <Image source={t.avatar} style={styles.responsavelImg} />
+                    <Text>{t.responsavel}</Text>
+                  </View>
+
+                </View>
+                <Text style={{ color: t.status === 'Atrasado' ? 'red' : 'green' }}>{t.status}</Text>
+              </View>
+            ))}
+          </View>
+
+          {/* Rankeamento */}
+          <View style={styles.rankingContainer}>
+            <View style={styles.rankingHeader}>
+              <Text style={styles.rankingTitle}>Rankeamento 🏆</Text>
+              <Text style={styles.metaText}>Meta: {casa.metaXp}xp</Text>
+            </View>
+
+            <View style={styles.podio}>
+              {/* 2º lugar */}
+              <View style={styles.rankBlock2}>
+                <Image source={{ uri: casa.ranking[1].avatar }} style={styles.avatar} />
+                <Text style={styles.rankName}>{casa.ranking[1].nome}</Text>
+                <View style={[styles.rankBar, { backgroundColor: '#D1D5DB', height: 60 }]}>
+                  <Text style={styles.rankXp}>{casa.ranking[1].xp}xp</Text>
+                </View>
+              </View>
+
+              {/* 1º lugar */}
+              <View style={styles.rankBlock1}>
+                <Image source={{ uri: casa.ranking[0].avatar }} style={styles.avatar} />
+                <Text style={styles.rankName}>
+                  {casa.ranking[0].nome} <Text>🥇</Text>
+                </Text>
+                <View style={[styles.rankBar, { backgroundColor: '#FACC15', height: 90 }]}>
+                  <Text style={styles.rankXp}>{casa.ranking[0].xp}xp</Text>
+                </View>
+              </View>
+
+              {/* 3º lugar */}
+              <View style={styles.rankBlock3}>
+                <Image source={{ uri: casa.ranking[2].avatar }} style={styles.avatar} />
+                <Text style={styles.rankName}>{casa.ranking[2].nome}</Text>
+                <View style={[styles.rankBar, { backgroundColor: '#D97706', height: 45 }]}>
+                  <Text style={styles.rankXp}>{casa.ranking[2].xp}xp</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+        </View>
+      </View >
+    </ScrollView>
   )
 }
---
+++ src/screens/usuario-com-casa-screen/styles.ts
@@ -0,0 +1,183 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F3F8FF",
+    marginTop: 24,
+  },
+  headerImage: {
+    width: "100%",
+    height: 150,
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 24
+  },
+  content: {
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 32,
+    borderTopRightRadius: 32,
+    padding: 24,
+    flex: 1,
+    paddingBottom: 60
+  },
+  nomeCasa: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    color: '#14532D'
+  },
+
+  codigoCasa: {
+    fontSize: 16,
+    color: '#463E95',
+    marginTop: 4,
+    fontWeight: '600'
+  },
+
+  membrosContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 24
+  },
+
+  membroItem: {
+    alignItems: 'center'
+  },
+
+  avatar: {
+    width: 58,
+    height: 58,
+    borderRadius: 24,
+    marginBottom: 4
+  },
+
+  tarefasContainer: {
+    marginBottom: 24
+  },
+
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 30,
+    color: '#166534'
+  },
+
+  novaTarefaButton: {
+    backgroundColor: '#E0E7FF',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    alignSelf: 'flex-end',
+    borderRadius: 6,
+    marginBottom: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10
+  },
+
+  novaTarefaText: {
+    color: '#4338CA',
+    fontWeight: '500'
+  },
+
+  tarefaItem: {
+    backgroundColor: '#E7F9F2',
+    padding: 12,
+    borderRadius: 20,
+    marginBottom: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.05,
+    shadowRadius: 2,
+    elevation: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 8
+  },
+
+  rankingContainer: {
+    marginBottom: 32
+  },
+
+  rankingItem: {
+    backgroundColor: '#F3F4F6',
+    borderRadius: 8,
+    padding: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8
+  },
+  tituloTarefa: {
+    fontSize: 16,
+    fontWeight: '600'
+  },
+  responsavelContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 7,
+    marginTop: 8
+  },
+  responsavelImg: {
+    width: 30,
+    height: 30
+  },
+  rankingHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 12
+  },
+
+  rankingTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#10B981'
+  },
+
+  metaText: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: '#7C3AED'
+  },
+
+  podio: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    gap: 12
+  },
+
+  rankBlock1: {
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+
+  rankBlock2: {
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+
+  rankBlock3: {
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+
+  rankBar: {
+    width: 60,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 4
+  },
+
+  rankName: {
+    fontSize: 14,
+    fontWeight: '600',
+    marginVertical: 4
+  },
+
+  rankXp: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#111827'
+  },
+})
